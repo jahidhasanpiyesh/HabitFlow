353c2f318aa73cc4c05d6f688936bab4b503bb5b
Md. Jahid Hasan <jahidhasanpiyesh@gmail.com>
2026-01-04 14:24:28 +0600

    comment added data and profesonal.

--- app1/views.py
+++ app1/views.py
@@ -9,17 +9,17 @@ def home(request):
         { "name": "Reading 20 Pages", "icon": "📚", "done": [1, 3, 5, 7, 9, 11], "progress": 15 },
     ]
     
-    # ২০২৬ সালের জানুয়ারি মাসের তারিখ এবং সঠিক বার বের করা
+    # mounting days data with day names
     days_data = []
     for d in range(1, 32):
         date_obj = datetime.date(2026, 1, d)
         days_data.append({
             'num': d,
-            'name': date_obj.strftime('%a')  # এটি Thu, Fri, Sat ইত্যাদি অটোমেটিক দেবে
+            'name': date_obj.strftime('%a')  # This data is automatic update date base Thu, Fri, Sat 
         })
 
     context = {
         'habits': demo_habits,
-        'days_data': days_data, # এটি ব্যবহার করলে লুপ ঠিক থাকবে
+        'days_data': days_data, # This data is used to loop through the days calender
     }
     return render(request, 'index.html', context)
