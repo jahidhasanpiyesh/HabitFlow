3474717a2379f23eb0a39985c5838fc9991ff352
Md. Jahid Hasan <jahidhasanpiyesh@gmail.com>
2026-01-04 13:56:19 +0600

    Data views dynamic way try and multi format used.

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    # Demo data for habits
+    demo_habits = [
+        { "name": "5:30 AM Wake Up", "icon": "⏰", "done": [1, 2, 3, 5, 7, 10, 12, 14, 15, 18], "progress": 45 },
+        { "name": "Deep Work (4hr)", "icon": "💻", "done": [1, 2, 4, 5, 8, 9, 11, 12, 15], "progress": 30 },
+        { "name": "Gym Workout", "icon": "🏋️‍♂️", "done": [2, 4, 6, 8, 10, 12, 14], "progress": 20 },
+        { "name": "Reading 20 Pages", "icon": "📚", "done": [1, 3, 5, 7, 9, 11], "progress": 15 },
+    ]
+    
+    context = {
+        'habits': demo_habits,
+        'days_range': range(1, 32), # Day numbers from 1 to 31
+        'days_name': ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    }
+    return render(request, 'index.html', context)
